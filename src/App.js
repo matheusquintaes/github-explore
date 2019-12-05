b4e8f00e21b3a058c68fcbb1dd7a076300097036
Matheus Quintaes <matheus.quintaes@gmail.com>
2019-12-05 20:08:06 +1300

    Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Header from './components/Header'
 import Layout from './components/Layout'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Results from './components/Battle/Results'
+import Loading from './components/Loading'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Battle/Results'))
 
 function App() {
   return (
     <Router>
       <Header/>
       <Layout>
-        <Switch>
-          <Route exact path='/' component={Popular} />
-          <Route exact path='/battle' component={Battle} />
-          <Route path='/battle/results' component={Results} />
-          <Route render={() => <h1>404</h1>} />
-        </Switch>
+        <React.Suspense fallback={<Loading />} >
+          <Switch>
+            <Route exact path='/' component={Popular} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
+            <Route render={() => <h1>404</h1>} />
+          </Switch>
+        </React.Suspense>
       </Layout>
     </Router>
   );
